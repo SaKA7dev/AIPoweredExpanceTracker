6d93dbc4a27f5f8959ceded67fad3e0af081eead
SaKA7dev <shivamkmishra0809@gmail.com>
2024-05-11 11:02:44 +0530

    Add files via upload

--- handler/user.handler.php
+++ handler/user.handler.php
@@ -1,1 +1,10 @@
+<?php
+require_once("../controller/user.controller.php");
+$METHOD = $_SERVER['REQUEST_METHOD'];
+echo $METHOD;
+$METHOD==="POST" && Post();
+$METHOD==="PUT" && Put();
+$METHOD==="GET" && Get();
+$METHOD==="DELETE"&& Delete();
 
+?>
